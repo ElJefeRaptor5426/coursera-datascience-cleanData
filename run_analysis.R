801b8697deb9e7b0e958d0e01e9e56680d88c118
El Jefe Raptor <apburgoon@gmail.com>
2017-11-16 13:55:25 -0500

    Third times a charm!

--- run_analysis.R
+++ run_analysis.R
@@ -12,9 +12,10 @@ feat_names <- make.names(names = features$V2, unique = TRUE)
 # Load test dataset
 test_data <- read.table("UCI HAR Dataset/test/X_test.txt")
 test_labels <- read.table("UCI HAR Dataset/test/y_test.txt")
+test_subjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
 # Add activity column to test_data
-test_data <- mutate(test_data, Activity = test_labels$V1)
+test_data <- mutate(test_data, Activity = test_labels$V1, Subject = test_subjects$V1)
 
 # Create factor for test dataset and activity labels
 test_data$Activity <- as.factor(test_data$Activity)
@@ -22,9 +23,10 @@ test_data$Activity <- as.factor(test_data$Activity)
 # Load train dataset
 train_data <- read.table("UCI HAR Dataset/train/X_train.txt")
 train_labels <- read.table("UCI HAR Dataset/train/y_train.txt")
+train_subjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
 # Add activity column to test_data
-train_data <- mutate(train_data, Activity = train_labels$V1)
+train_data <- mutate(train_data, Activity = train_labels$V1, Subject = train_subjects$V1)
 
 # Create factor for test dataset and activity labels
 train_data$Activity <- as.factor(train_data$Activity)
@@ -32,11 +34,12 @@ train_data$Activity <- as.factor(train_data$Activity)
 # make final dataset
 dataset <- bind_rows(test_data,train_data)
 levels(dataset$Activity) <- activities$Activity.Name
-names(dataset) <- c(feat_names, "Activity")
+names(dataset) <- c(sub("^(.*)\\.+(mean,std)\\.+(XYZ)$", "\\1.\\3.\\2",feat_names), "Activity", "Subject")
 
 meanSet <- select(dataset,contains("mean"))
 stdSet <- select(dataset, contains("std"))
 actSet <- dataset$Activity
-dataset <- bind_cols(meanSet, stdSet, data.frame(Activity=actSet))
+subjSet <- dataset$Subject
+dataset <- bind_cols(meanSet, stdSet, data.frame(Activity=actSet), data.frame(Subject=subjSet))
 
-act_dataset <- aggregate(dataset[1:86],dataset[87],mean)
+final_dataset <- aggregate(dataset[1:86],dataset[87:88],mean)
